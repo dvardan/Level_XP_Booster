a886de4b616873be1cffc1208d04cbc8000daa33
dvardan <33897246+dvardan@users.noreply.github.com>
2017-11-22 16:30:31 +0400

    version 0.2

--- LevelXPBooster/app/src/main/java/com/level/xp/booster/levelxpbooster/MainActivity.java
+++ LevelXPBooster/app/src/main/java/com/level/xp/booster/levelxpbooster/MainActivity.java
@@ -1,13 +1,195 @@
 package com.level.xp.booster.levelxpbooster;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+
+
+public class MainActivity extends AppCompatActivity implements RewardedVideoAdListener{
+    Button pressButton;
+    int k;
+
+
+
+    private RewardedVideoAd mRewardedVideoAd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences sp = getSharedPreferences("your_prefs", Activity.MODE_PRIVATE);
+        int myIntValue = sp.getInt("points", 0);
+        k = myIntValue;
+
+        pressButton = (Button)findViewById(R.id.pressButton);
+        if(k != 0){
+            pressButton.setText(Integer.toString(k));
+        }
+        pressButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                k++;
+                pressButton.setText(Integer.toString(k));
+
+            }
+        });
+
+        final AdView adView = (AdView)findViewById(R.id.adView);
+
+        //Change to real banner
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice("33BE2250B43518CCDA7DE426D04EE232").build();
+        adView.loadAd(adRequest);
+
+
+        mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this);
+        mRewardedVideoAd.setRewardedVideoAdListener(this);
+
+        loadRewardedVideoAd();
+
+        ImageView splash = (ImageView)findViewById(R.id.bigSplash);
+        splash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mRewardedVideoAd.isLoaded()) {
+                    mRewardedVideoAd.show();
+
+                }
+
+            }
+        });
+        ImageView splash1 = (ImageView)findViewById(R.id.splash1);
+        splash1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mRewardedVideoAd.isLoaded()) {
+                    mRewardedVideoAd.show();
+
+                }
+
+            }
+        });
+        ImageView splash2 = (ImageView)findViewById(R.id.splash2);
+        splash2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mRewardedVideoAd.isLoaded()) {
+                    mRewardedVideoAd.show();
+
+                }
+
+            }
+        });
+
+
+
+
+    }
+
+
+    private void loadRewardedVideoAd() {
+        mRewardedVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917",
+                new AdRequest.Builder().build());
+    }
+
+
+    @Override
+    public void onRewardedVideoAdLoaded() {
+
+        System.out.println("onRewardedVideoAdLoaded");
+
+    }
+
+    @Override
+    public void onRewardedVideoAdOpened() {
+
+        System.out.println("onRewardedVideoAdOpened");
+
+    }
+
+    @Override
+    public void onRewardedVideoStarted() {
+        System.out.println("onRewardedVideoStarted");
+
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+        System.out.println("onRewardedVideoAdClosed");
+        mRewardedVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917",
+                new AdRequest.Builder().build());
+
+    }
+
+    @Override
+    public void onRewarded(RewardItem rewardItem) {
+        System.out.println("onRewarded");
+        mRewardedVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917",
+                new AdRequest.Builder().build());
+        k = k + 200;
+        pressButton.setText(Integer.toString(k));
+
+
+
+    }
+
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+        System.out.println("onRewardedVideoAdLeftApplication");
+        mRewardedVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917",
+                new AdRequest.Builder().build());
+
+
+    }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int i) {
+        System.out.println("onRewardedVideoAdFailedToLoad");
+        mRewardedVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917",
+                new AdRequest.Builder().build());
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SharedPreferences sp = getSharedPreferences("your_prefs", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt("points", k);
+        editor.commit();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences sp = getSharedPreferences("your_prefs", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt("points", k);
+        editor.commit();
+
     }
 }
+
+
