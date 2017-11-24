fd420b233c8244fa59dddd56653e681902b37db4
dvardan <33897246+dvardan@users.noreply.github.com>
2017-11-24 19:47:54 +0400

    Version yesim inch
    Xar@ mar@ changes

--- LevelXPBooster/app/src/main/java/com/level/xp/booster/levelxpbooster/MainActivity.java
+++ LevelXPBooster/app/src/main/java/com/level/xp/booster/levelxpbooster/MainActivity.java
@@ -3,42 +3,93 @@ package com.level.xp.booster.levelxpbooster;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
-
-
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.reward.RewardItem;
 import com.google.android.gms.ads.reward.RewardedVideoAd;
 import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+import com.google.android.gms.auth.api.signin.*;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import android.support.annotation.NonNull;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.AchievementsClient;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.achievement.Achievement;
+import com.google.android.gms.games.achievement.AchievementBuffer;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
+import java.nio.BufferUnderflowException;
 
 public class MainActivity extends AppCompatActivity implements RewardedVideoAdListener{
     Button pressButton;
-    int k;
-
-
-
+    private int k;
+    private static final int RC_SIGN_IN = 1;
+    private GoogleSignInClient mClient;
     private RewardedVideoAd mRewardedVideoAd;
+    private GoogleApiClient mApiClient;
+
+    private static final int RC_ACHIEVEMENT_UI = 9003;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mClient = GoogleSignIn.getClient(MainActivity.this, gso);
+
+        if(!isSignedIn()){
+            signIn();
+
+
+
+
+        }
+        else{
+
+        }
+
+
+        Button ach = (Button)findViewById(R.id.ach);
+        ach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(MainActivity.this);
+                //Games.getAchievementsClient(MainActivity.this,acct).unlock(getString(R.string.achievement_5_click));
+
+             /*   Nar urem@ es function@ kancheluc error a tali, chem jogum inch a
+                bayc pti achievmentneri window baci
+                ha u indz tvuma et client id maydii het a kapvac
+                es functionn el amenatakn em decler arel*/
+                showAchievements();
+
+            }
+        });
+
+
+
+
+
+
+
+
         SharedPreferences sp = getSharedPreferences("your_prefs", Activity.MODE_PRIVATE);
         int myIntValue = sp.getInt("points", 0);
         k = myIntValue;
@@ -106,6 +157,7 @@ public class MainActivity extends AppCompatActivity implements RewardedVideoAdLi
 
 
     }
+
 
 
     private void loadRewardedVideoAd() {
@@ -191,6 +243,91 @@ public class MainActivity extends AppCompatActivity implements RewardedVideoAdLi
         editor.commit();
 
     }
+
+
+    //// Methods For Sign In
+    private void signIn() {
+        Intent signInIntent = mClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+
+        }
+    }
+
+
+
+
+
+
+
+    private void signInSilently() {
+        GoogleSignInClient signInClient = GoogleSignIn.getClient(this,
+                GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN);
+        signInClient.silentSignIn().addOnCompleteListener(this,
+                new OnCompleteListener<GoogleSignInAccount>() {
+                    @Override
+                    public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
+                        if (task.isSuccessful()) {
+
+                            GoogleSignInAccount signedInAccount = task.getResult();
+                            Toast.makeText(MainActivity.this,signedInAccount.getEmail().toString(), Toast.LENGTH_LONG).show();
+
+                        } else {
+                            // Player will need to sign-in explicitly using via UI
+                        }
+                    }
+                });
+    }
+    private boolean isSignedIn(){
+        return GoogleSignIn.getLastSignedInAccount(this) != null;
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //signInSilently();
+    }
+
+
+/////////////////////////////////////////////////
+    private void showAchievements() {
+        Games.getAchievementsClient(this, GoogleSignIn.getLastSignedInAccount(this))
+            .getAchievementsIntent()
+            .addOnSuccessListener(new OnSuccessListener<Intent>() {
+                @Override
+                public void onSuccess(Intent intent) {
+                    startActivityForResult(intent, RC_ACHIEVEMENT_UI);
+                }
+            });
+
+
+}
+
+
+
 }
 
 
